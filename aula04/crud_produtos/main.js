cc1cfd0c6dee7f456a17113f682b3f5699a6697f
JOÃO LUIS ROSA DE MOURA <632310027@fspoa.br>
2024-03-26 21:18:09 -0300

    2024-03-26 Orientações do Jack

--- aula04/crud_produtos/main.js
+++ aula04/crud_produtos/main.js
@@ -13,11 +13,29 @@ let listaProdutos = [
     }
 ];
 
+let idGenerador = 2;
+
 function listar(){
     return listaProdutos;
 }
 
+function inserir(produto){
+    produto.id = idGenerador++;
+    listaProdutos.push(produto);
+}
+
 function main(){
+    console.log(listar());
+    inserir({
+        nome: "Feijão"
+        ,categoria: "alimento"
+        ,preco: 8.00
+    });
+    inserir({
+        nome: "Suco de laranja"
+        ,categoria: "bebida"
+        ,preco: 9.20
+    });
     console.log(listar());
 }
 
